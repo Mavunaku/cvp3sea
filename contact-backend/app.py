4123c0a512a371648d4570a76269107da0e34bdd
Mavunaku <mavunaku@gmail.com>
2025-11-18 17:57:22 -0500

    Add Flask backend and requirements for Render deployment

---
+++ contact-backend/app.py
@@ -0,0 +1,47 @@
+from flask import Flask, request, jsonify
+import smtplib
+from email.mime.text import MIMEText
+import os
+
+app = Flask(__name__)
+
+SMTP_SERVER = "smtp.gmail.com"
+SMTP_PORT = 587
+SMTP_USER = os.getenv("SMTP_USER")
+SMTP_PASS = os.getenv("SMTP_PASS")
+
+RECIPIENTS = os.getenv("RECIPIENTS").split(",")
+
+@app.route("/send-message", methods=["POST"])
+def send_message():
+    name = request.form.get("name")
+    email = request.form.get("email")
+    message = request.form.get("message")
+
+    body = f"""
+New Contact Form Submission
+--------------------------
+
+Name: {name}
+Email: {email}
+
+Message:
+{message}
+"""
+
+    msg = MIMEText(body)
+    msg["Subject"] = "New Contact Form Message"
+    msg["From"] = SMTP_USER
+    msg["To"] = ", ".join(RECIPIENTS)
+
+    try:
+        server = smtplib.SMTP(SMTP_SERVER, SMTP_PORT)
+        server.starttls()
+        server.login(SMTP_USER, SMTP_PASS)
+        server.sendmail(SMTP_USER, RECIPIENTS, msg.as_string())
+        server.quit()
+    except Exception as e:
+        print("Email Failed:", e)
+        return jsonify({"status": "error", "message": "Email failed"}), 500
+
+    return jsonify({"status": "success"})
